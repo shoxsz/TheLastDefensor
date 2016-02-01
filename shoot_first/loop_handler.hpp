e26262b8349a9258ebefd60f3845b0d8f6b3b39e
Paulo Márcio <paulomarcio18p1@gmail.com>
2016-01-31 23:44:29 -0200

    transforming in a pure virtual class

--- shoot_first/loop_handler.hpp
+++ shoot_first/loop_handler.hpp
@@ -6,11 +6,11 @@
 class LoopHandler{
 public:
 
-	virtual void pre(){}
-	virtual void process(SDL_Event* event){}
-	virtual void logics(){}
-	virtual void render(){}
-	virtual void pos(){}
+	virtual void pre() = 0;
+	virtual void process(SDL_Event* event) = 0;
+	virtual void logics() = 0;
+	virtual void render() = 0;
+	virtual void pos() = 0;
 
 	virtual bool active(){ return true; }
 
